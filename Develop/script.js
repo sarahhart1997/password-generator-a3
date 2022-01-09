8c2c6ac770a8d0d768080ad65b7813d42945f1ca
Sarah Hart <sarah.hart.nc@gmail.com>
2022-01-09 14:31:00 -0500

    first commit

--- Develop/script.js
+++ Develop/script.js
@@ -15,3 +15,24 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//Prompted for the length of the password. 
+
+//Choose a length of at least 8 characters and less than 128 characters. 
+
+//Prompt to include lowercase letters. 
+
+// Confirm yes or no to lowercase letters. 
+
+//Prompt to include uppercase letters. 
+
+//Confirm yes or no to uppercase letters. 
+
+//Prompt to include numeric letters. 
+
+//Confirm yes or no to include numeric letters. 
+
+//Prompt to include special characters 
+
+//Confirm yes or no to include special characters. 
+
